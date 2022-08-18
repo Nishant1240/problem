db41f07b695f9d14f8394ee90263f884406edc58
Nishant Solanki <info.tech1240@gmail.com>
2022-08-18 06:41:44 +0530

    fix

--- post.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-Type: application/json');
-// $data = json_decode(json_encode($_POST['data']));
-// $response['data'] = print_r($data);
-
-$response['method'] = 'POST';
-echo json_encode($response);
