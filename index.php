9837521e26ddfd7129be00bfbb8502b9b4163b4b
Nishant Solanki <info.tech1240@gmail.com>
2022-08-19 17:38:03 +0530

    initail

--- index.php
+++ index.php
@@ -5,13 +5,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Problem demo</title>
+    <title>Root</title>
 </head>
 
 <body>
-    <h1>
-        Clone Repo. this is update version 2. but this demo
-    </h1>
+    <h1>Root Project</h1>
 </body>
 
 </html>
